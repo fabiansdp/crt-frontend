d5ac46774e2df6756fade7705cb8a3401b88d5e8
Fabian <13519140@std.stei.itb.ac.id>
2021-06-02 13:23:58 +0700

    add axios and connect to backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import {useState} from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
+  const [response, setResponse] = useState<string>();
+
+  axios.get("http://localhost:8080/")
+  .then(response => {
+    setResponse(response.data);
+  }).catch(e => {
+    console.log(e);
+  })
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +20,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>{response}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
